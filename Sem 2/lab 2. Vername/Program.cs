591c67163f1237590cb7ca212fc4e61b5ca65c29
Кривошея Михаил <mrkriv@bk.ru>
2017-12-08 20:41:16 +0300

    Add 2 and 3 labs

---
+++ Sem 2/lab 2. Vername/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Vername
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rand = new Random();
+            var input = "I love SPO";
+
+            var key = string.Concat(Enumerable.Range(0, input.Length).Select(x => (char)rand.Next('A', 'Z')));
+
+            var code = EncodeOrDecode(input, key);
+            var decode = EncodeOrDecode(code, key);
+
+            Console.WriteLine($"Source: {input}");
+            Console.WriteLine($"Key: {key}");
+            Console.WriteLine($"Encode: {code}");
+            Console.WriteLine($"Decode: {decode}");
+        }
+
+        public static string EncodeOrDecode(string input, string key)
+        {
+            return string.Concat(input.Select((x, i) => (char) (x ^ key[i])));
+        }
+    }
+}
---
+++ Sem 2/lab 3. Diffi-Khellmana/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+
+namespace Diffi_Khellmana
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var medved = new Entity("Медведев");
+            var naval = new Entity("Навальный");
+
+            medved.Join(naval);
+            naval.Join(medved);
+
+            medved.Recive(naval.Send("Отвечай за виноградки!"));
+            naval.Recive(medved.Send("Omae wa mou shindeiru!"));
+            medved.Recive(naval.Send("NANI ?!"));
+        }
+    }
+
+    class Entity
+    {
+        private const int PrivateKeyLen = 32;
+        private const int Root = 3;
+        private const int PublicConst = 17;
+
+        private readonly string _privateKey;
+        private readonly string _name;
+
+        public string PublicMyKey { get; }
+        public string PublicOtherKey { get; set; }
+
+        public Entity(string name)
+        {
+            _name = name;
+            var rand = new Random();
+
+            _privateKey = string.Concat(Enumerable.Range(0, PrivateKeyLen).Select(x => (char) rand.Next('A', 'Z')));
+            PublicMyKey = string.Concat(_privateKey.Select(x =>
+                NormalizeChar(Math.Pow(Root, x) % PublicConst)));
+
+            Console.WriteLine($"{name} have private key {_privateKey}");
+            Console.WriteLine($"{name} have public key {PublicMyKey}");
+        }
+
+        public void Join(Entity other)
+        {
+            PublicOtherKey = other.PublicMyKey;
+        }
+
+        public string Send(string message)
+        {
+            var encodeMessage = Magic(message);
+            Console.WriteLine($"{_name}: {message} -> {encodeMessage}");
+
+            return encodeMessage;
+        }
+
+        public string Recive(string message)
+        {
+            var decodeMessage = Magic(message);
+            Console.WriteLine($"{_name}: {message} -> {decodeMessage}");
+
+            return decodeMessage;
+        }
+
+        private string Magic(string message)
+        {
+            var key = string.Concat(PublicOtherKey.Select((x, i) => Math.Pow(Root, _privateKey[i] * x) % PublicConst));
+            return string.Concat(message.Select((x, i) => (char) (x ^ key[i % key.Length])));
+        }
+
+        private static char NormalizeChar(double input)
+        {
+            return (char) ('A' + input % ('Z' - 'A'));
+        }
+    }
+}
